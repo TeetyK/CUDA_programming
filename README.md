e93cf8619cc8eedf1edb646769246180f5fbe8c3
TeetyK <teetysupergame7484@gmail.com>
2026-03-08 21:40:57 +0700

    init project learning

---
+++ README.md
@@ -0,0 +1,9 @@
+Compile C++
+```bash
+g++ --fiile.cpp -o --output.exe
+.\output
+```
+Compile NVCC CUDA
+```bash
+nvcc -arch=sm_86 -o file_cu file.cu
+```
---
+++ cuda_test.cu
@@ -0,0 +1,10 @@
+#include <cuda_runtime.h>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "cuda runtime work?" <<endl;
+    return 0;
+}
